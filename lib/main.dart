2f8e396db8e36784a86cddf85bab426441e83986
Bruce Zhang <zttt183525594@gmail.com>
2024-09-10 16:49:49 +0800

    use dynamic material color on supported platforms

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:dynamic_color/dynamic_color.dart';
 import 'package:feeluownx/pages/playlist_ui.dart';
 import 'package:feeluownx/search.dart';
 import 'package:feeluownx/pages/settings.dart';
@@ -29,6 +30,12 @@ class AppState extends State<App> with SingleTickerProviderStateMixin {
 
   late TabController tabController;
 
+  // default theme when dynamic color cannot be found
+  static final _defaultLightColorScheme =
+      ColorScheme.fromSwatch(primarySwatch: Colors.blue);
+  static final _defaultDarkColorScheme = ColorScheme.fromSwatch(
+      primarySwatch: Colors.blue, brightness: Brightness.dark);
+
   @override
   void initState() {
     super.initState();
@@ -37,36 +44,45 @@ class AppState extends State<App> with SingleTickerProviderStateMixin {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      localizationsDelegates: AppLocalizations.localizationsDelegates,
-      supportedLocales: AppLocalizations.supportedLocales,
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text('FeelUOwn'),
+    return DynamicColorBuilder(builder: (lightColorScheme, darkColorScheme) {
+      return MaterialApp(
+        localizationsDelegates: AppLocalizations.localizationsDelegates,
+        supportedLocales: AppLocalizations.supportedLocales,
+        home: Scaffold(
+          appBar: AppBar(
+            title: const Text('FeelUOwn'),
+          ),
+          body: TabBarView(
+              controller: tabController,
+              physics: const NeverScrollableScrollPhysics(),
+              children: children),
+          bottomNavigationBar: BottomNavigationBar(items: const [
+            BottomNavigationBarItem(icon: Icon(Icons.home), label: "Home"),
+            BottomNavigationBarItem(icon: Icon(Icons.list), label: "Playing"),
+            BottomNavigationBarItem(
+                icon: Icon(Icons.settings), label: "Settings")
+          ], currentIndex: currentIndex, onTap: onTabChange),
+          floatingActionButton: Builder(
+              builder: (context) => FloatingActionButton(
+                  onPressed: () async {
+                    await showSearch(
+                        context: context,
+                        delegate: Global.getIt<SongSearchDelegate>());
+                  },
+                  child: const Icon(Icons.search))),
         ),
-        body: TabBarView(
-            controller: tabController,
-            physics: const NeverScrollableScrollPhysics(),
-            children: children),
-        bottomNavigationBar: BottomNavigationBar(items: const [
-          BottomNavigationBarItem(icon: Icon(Icons.home), label: "Home"),
-          BottomNavigationBarItem(icon: Icon(Icons.list), label: "Playing"),
-          BottomNavigationBarItem(icon: Icon(Icons.settings), label: "Settings")
-        ], currentIndex: currentIndex, onTap: onTabChange),
-        floatingActionButton: Builder(
-            builder: (context) => FloatingActionButton(
-                onPressed: () async {
-                  await showSearch(
-                      context: context,
-                      delegate: Global.getIt<SongSearchDelegate>());
-                },
-                child: const Icon(Icons.search))),
-      ),
-      // auto dark mode follows system settings
-      themeMode: ThemeMode.system,
-      theme: ThemeData.light(useMaterial3: true),
-      darkTheme: ThemeData.dark(useMaterial3: true),
-    );
+        // auto dark mode follows system settings
+        themeMode: ThemeMode.system,
+        theme: ThemeData(
+          colorScheme: lightColorScheme ?? _defaultLightColorScheme,
+          useMaterial3: true,
+        ),
+        darkTheme: ThemeData(
+          colorScheme: darkColorScheme ?? _defaultDarkColorScheme,
+          useMaterial3: true,
+        ),
+      );
+    });
   }
 
   void onTabChange(int index) {
--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -7,6 +7,7 @@ import Foundation
 
 import audio_service
 import audio_session
+import dynamic_color
 import path_provider_foundation
 import shared_preferences_foundation
 import sqflite
@@ -14,6 +15,7 @@ import sqflite
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   AudioServicePlugin.register(with: registry.registrar(forPlugin: "AudioServicePlugin"))
   AudioSessionPlugin.register(with: registry.registrar(forPlugin: "AudioSessionPlugin"))
+  DynamicColorPlugin.register(with: registry.registrar(forPlugin: "DynamicColorPlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
   SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
   SqflitePlugin.register(with: registry.registrar(forPlugin: "SqflitePlugin"))
